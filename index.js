4855d17c6b7ae1f66d3558904c7a91d2f4552f06
Adrian de Cléir <adrian.decleir@devhouse.ie>
2023-01-07 13:39:56 +0000

    passing multi array and unencrypted value in

--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 // automatically decrypts the data and maintains its structure so you can treat event exactly as 
 // you did when you passed it into `evervault.run`.
 exports.handler = async (data, context) => {
+	
+	let returnString = '';
+	
+	for(let i = 0;i<data.length;i++) {
+		returnString += ` name: ${data[i].name} \n`;
+	}
+	
+    return {
+        message: returnString,
+        name: context.encrypt(data.name),
+    };	
+	
     // Check if the data sent into the Function included the `name` key
     if (data.name && typeof data.name === "string") {
         console.debug(`A name of length ${data.name.length} has arrived into the Function.`);
     
         // Process the decrypted name value, and re-encrypt the original name using the encrypt function available in the context parameter.
-        return {
-            message: `Hello from a Function! It seems you have ${data.name.length} letters in your name, actually the name is ${data.name}`,
-            name: context.encrypt(data.name),
-        };
+
     } else {
         console.debug('An empty name has arrived into the Function.');
     
